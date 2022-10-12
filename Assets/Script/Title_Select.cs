ce0bd3280ec1f80edbfbb136e7160cccbb9e326e
Rlan498 <harun.rs46@icloud.com>
2022-10-12 22:45:49 +0900

    操作説明の追加

--- Assets/Script/Title_Select.cs
+++ Assets/Script/Title_Select.cs
@@ -15,8 +15,19 @@ public class Title_Select : MonoBehaviour
     GameObject Exit;
     GameObject Arrow;
     GameObject bgm_2;
+    public GameObject exp1_1;
+    public GameObject exp1_2;
+    public GameObject exp1_3;
+    public GameObject exp2_1;
+    public GameObject exp2_2;
+    public GameObject exp2_3;
+    public GameObject exp2_4;
+    public GameObject pointer;
+    public GameObject exp_dis;
 
     int push = 0;
+    int place2 = 0;
+    bool exp_open = false;
 
     // Start is called before the first frame update
     void Start()
@@ -34,45 +45,71 @@ public class Title_Select : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(push != 1)
+        if(exp_open == false)
         {
-            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))//選択肢　下に
+            if (push != 1)
             {
-                place += 1;
-            }
-            else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))//選択肢　上に
-            {
-                place -= 1;
-            }
-            else if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
-            {//決定が押された時の処理
-                if (place == 0)//始めるを選択
+                if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))//選択肢　下に
                 {
-                    push = 1;
-                    this.GetComponent<AudioSource>().Play();
-                    StartCoroutine("wait");
+                    place += 1;
                 }
-                else if (place == 1)//操作説明を選択
+                else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))//選択肢　上に
                 {
-                    push = 1;
-                    this.GetComponent<AudioSource>().Play();
+                    place -= 1;
                 }
-                else if (place == 2)//ゲーム終了を選択
-                {
-                    //UnityEditor.EditorApplication.isPlaying = false;
-                    Application.Quit();
+                else if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
+                {//決定が押された時の処理
+                    if (place == 0)//始めるを選択
+                    {
+                        push = 1;
+                        this.GetComponent<AudioSource>().Play();
+                        StartCoroutine("wait");
+                    }
+                    else if (place == 1)//操作説明を選択
+                    {
+                        push = 1;
+                        exp_open = true;
+                        this.GetComponent<AudioSource>().Play();
+                        exp_dis.SetActive(true);
+                    }
+                    else if (place == 2)//ゲーム終了を選択
+                    {
+                        //UnityEditor.EditorApplication.isPlaying = false;
+                        Application.Quit();
+                    }
                 }
-            }
 
-            if (place > 2)//下から上に　上から下に行くための処理
-            {
-                place = 0;
-            }
-            else if (place < 0)
-            {
-                place = 2;
+                if (place > 2)//下から上に　上から下に行くための処理
+                {
+                    place = 0;
+                }
+                else if (place < 0)
+                {
+                    place = 2;
+                }
             }
         }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))//選択肢　右に
+            {
+                place2 += 1;
+                StartCoroutine("exp_disp");
+            }
+            else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))//選択肢　左に
+            {
+                place2 -= 1;
+                StartCoroutine("exp_disp");
+            }
+            else if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
+            {
+                exp_dis.SetActive(false);
+                place2 = 0;
+                push = 0;
+                exp_open = false;
+            }  
+        }
+        
     }
 
     private IEnumerator wait()
@@ -141,5 +178,82 @@ public class Title_Select : MonoBehaviour
         Result.stop = 0;
 
         SceneManager.LoadScene("SampleScene");
+    }
+
+    private IEnumerator exp_disp()
+    {
+        exp1_1.SetActive(false);
+        exp1_2.SetActive(false);
+        exp1_3.SetActive(false);
+        exp2_1.SetActive(false);
+        exp2_2.SetActive(false);
+        exp2_3.SetActive(false);
+        exp2_4.SetActive(false);
+
+        if (place2 > 6)//右から左に　左から右に行くための処理
+        {
+            place2 = 0;
+        }
+        else if (place2 < 0)
+        {
+            place2 = 6;
+        }
+
+        if (place2 == 0)
+        {
+            yield return new WaitForSeconds(0.001f);
+            exp1_1.SetActive(true);//1-1表示
+
+            pointer.transform.position = new Vector2(-3, -4.5f);
+            Debug.Log("1番目表示");
+        }
+        else if (place2 == 1)
+        {
+            yield return new WaitForSeconds(0.001f);
+            exp1_2.SetActive(true);//1-2表示
+
+            pointer.transform.position = new Vector2(-2, -4.5f);
+            Debug.Log("2番目表示");
+        }
+        else if (place2 == 2)
+        {
+            yield return new WaitForSeconds(0.001f);
+            exp1_3.SetActive(true);//1-3表示
+
+            pointer.transform.position = new Vector2(-1, -4.5f);
+            Debug.Log("3番目表示");
+        }
+        else if (place2 == 3)
+        {
+            yield return new WaitForSeconds(0.001f);
+            exp2_1.SetActive(true);//2-1表示
+
+            pointer.transform.position = new Vector2(0, -4.5f);
+            Debug.Log("4番目表示");
+        }
+        else if (place2 == 4)
+        {
+            yield return new WaitForSeconds(0.001f);
+            exp2_2.SetActive(true);//2-2表示
+
+            pointer.transform.position = new Vector2(1, -4.5f);
+            Debug.Log("5番目表示");
+        }
+        else if (place2 == 5)
+        {
+            yield return new WaitForSeconds(0.001f);
+            exp2_3.SetActive(true);//2-3表示
+
+            pointer.transform.position = new Vector2(2, -4.5f);
+            Debug.Log("6番目表示");
+        }
+        else if (place2 == 6)
+        {
+            yield return new WaitForSeconds(0.001f);
+            exp2_4.SetActive(true);//2-4表示
+
+            pointer.transform.position = new Vector2(3, -4.5f);
+            Debug.Log("7番目表示");
+        }
     }
 }
